f2ef9600740edf95c802f6c22e9daf2c934ba0c3
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-11 20:55:34 +0700

    feat: Add page title to About component

--- src/pages/About/views/About.jsx
+++ src/pages/About/views/About.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <Helmet>
+        <title>About Us - Narrative</title>
+      </Helmet>
+    </div>
+  );
 }
