e215ac1cbb8eebc94f2c97ed3d5f7d257944eba7
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-11 14:43:11 +0700

    feat: Add HelpCenter and HelpCenterBot page components for customer support

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,15 @@ import Dashboard from "./Dashboard/views/Dashboard";
 import About from "./About/views/About";
 import Contact from "./Contact/views/Contact";
 import Novels from "./Novels/views/Novels";
+import HelpCenter from "./HelpCentre/views/HelpCenter";
+import HelpCenterBot from "./HelpCentre/views/HelpCenterBot";
 
-export { Home, Dashboard, About, Contact, Novels };
+export {
+  Home,
+  Dashboard,
+  About,
+  Contact,
+  Novels,
+  HelpCenter,
+  HelpCenterBot,
+};
